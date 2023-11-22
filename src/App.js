d6fb31dd673547d1ab4314703c20f8664a84d662
Fahad Shahzad <75601362+fahad-qureshi786@users.noreply.github.com>
2023-11-22 16:31:35 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Route, Routes} from "react-router-dom";
+import HeroSection from "./components/HeroSection";
+
+export default function App() {
+    return (
+        <div>
+            {/*JSX */}
+            <Header/>
+                <Routes>
+                    <Route path={"/"} element={<HeroSection />}></Route>
+                </Routes>
+            <Footer/>
+
+        </div>
+    );
+}
